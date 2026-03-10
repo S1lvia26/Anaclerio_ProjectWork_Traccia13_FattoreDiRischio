abced2604e7145b330a2c0378ffa5c8069a0bebe
Silvia <silvia_anaclerio@libero.it>
2026-03-10 20:01:01 +0100

    creazione pagina matrice e generatore qr code

---
+++ myScript.js
@@ -0,0 +1,6 @@
+// genera il QR code
+new QRCode(document.getElementById("qrcode"), {
+    text: "https://s1lvia26.github.io/Basic_calculator_test/esercizio12/ciao.html",  // da aggiustare link e fare git pages
+    width: 150,
+    height: 150
+});
