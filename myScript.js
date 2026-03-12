e79649b781b8eab1bfe28069dcc33101a2a088eb
Silvia <silvia_anaclerio@libero.it>
2026-03-12 17:25:29 +0100

    migliorato layout e pagina home

--- myScript.js
+++ myScript.js
@@ -1,15 +1,5 @@
-// genera il QR code
-/*
-new QRCode(document.getElementById("qrcode"), {
-    text: "https://s1lvia26.github.io/Basic_calculator_test/esercizio12/ciao.html",  // da aggiustare link e fare git pages
-    width: 150,
-    height: 150
-});
-*/
-
-// calcolo
-
 document.addEventListener("DOMContentLoaded", function() {
+
     const qrElement = document.getElementById("qrcode");
     if(qrElement){
         // codice generazione QR
@@ -22,21 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("QR element non presente, salto questo codice");
     }
     
-
-
-    console.log('ciao');
-    document.querySelectorAll("select.pericolo, select.gravita").forEach(select => {
+    document.querySelectorAll("select.probabilita, select.gravita").forEach(select => {
         select.addEventListener("change", function() {
 
             // Trova la riga di questa select
             const row = this.closest("tr");
 
             // Prendi i valori delle due select della stessa riga
-            const pericolo = Number(row.querySelector("select.pericolo").value) || 0;
+            const probabilita = Number(row.querySelector("select.probabilita").value) || 0;
             const gravita = Number(row.querySelector("select.gravita").value) || 0;
 
             // Calcola il rischio
-            const rischio = pericolo * gravita;
+            const rischio = probabilita * gravita;
 
             // Aggiorna l'input della colonna rischio
             row.querySelector("input.rischio").value = rischio;
@@ -47,23 +34,3 @@ document.addEventListener("DOMContentLoaded", function() {
     
 });
 
-
-/*
-let num1= document.querySelector('#postazione_pericolo').value;
-console.log(num1);
-let num2= document.querySelector('#postazione_gravita').value;
-console.log(num2);
-let calcola3=document.querySelector('#calcola3');
-let total= document.querySelector('#postazione_rischio');
-
-function moltiplicazione (valore1,valore2) {
-    let moltiplicazione= Number(valore1) * Number(valore2);
-    return moltiplicazione;
-}
-
-
-calcola3.addEventListener('click',()=>{
-    total.value = moltiplicazione(num1.value,num2.value);
-
-});
-*/
