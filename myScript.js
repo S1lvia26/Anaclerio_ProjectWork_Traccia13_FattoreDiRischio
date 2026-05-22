e3a4d0d4780df77da02bc5d8cb47ed66eb4d42d3
Silvia <silvia_anaclerio@libero.it>
2026-05-22 17:39:06 +0200

    inserito colore qr code

--- myScript.js
+++ myScript.js
@@ -6,7 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
         new QRCode(document.getElementById("qrcode"), {
             text: "https://s1lvia26.github.io/Basic_calculator_test/esercizio12/ciao.html",  // da aggiustare link e fare git pages
             width: 150,
-            height: 150
+            height: 150,
+            colorDark: '#34a0a4'
+            
+            
         });
     } else {
         console.log("QR element non presente, salto questo codice");
