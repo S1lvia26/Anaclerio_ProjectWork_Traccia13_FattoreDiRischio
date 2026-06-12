1ced26b69fc11cd42034bb9408e3656cc227ae92
Silvia <silvia_anaclerio@libero.it>
2026-06-12 15:09:12 +0200

    rifinita pagina matrice affinche fosse piu mobile friendly

--- myScript.js
+++ myScript.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if(qrElement){
         // codice generazione QR
         new QRCode(document.getElementById("qrcode"), {
-            text: "https://s1lvia26.github.io/Anaclerio_ProjectWork_Traccia13_FattoreDiRischio/PDF_Informativo.pdf",
+            text: "https://s1lvia26.github.io/Anaclerio_ProjectWork_Traccia13_FattoreDiRischio/matrice.html",
             width: 150,
             height: 150,
             colorDark: '#153e6c'
